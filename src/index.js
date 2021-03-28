49052e4fb251a0a5fc699d1e8d1ddacf38956d35
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-03-28 19:11:50 +0200

    Fjernet sentry

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { worker } from "./mocks/browser";
-import * as Sentry from "@sentry/browser";
 import App from "./App";
 
 if (process.env.NODE_ENV === "development") {
   worker.start();
-} else {
-  // Sentry.init({ dsn: "TODO: Legg inn din  egen sentry url" });
 }
 
 ReactDOM.render(
